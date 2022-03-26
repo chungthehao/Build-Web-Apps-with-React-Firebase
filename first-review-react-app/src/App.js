ec4d6806d78144fce37769c0df3a1c735cc75d8f
Henry Chung <chungthehao@gmail.com>
2022-03-26 23:38:38 +0700

    Outputting lists

--- first-review-react-app/src/App.js
+++ first-review-react-app/src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
-  let [name, setName] = useState('Henry')
+  const [name, setName] = useState('Henry')
+  const [events, setEvents] = useState([
+    {title: 'Event 1', id: 1},
+    {title: 'Event 2', id: 2},
+    {title: 'Event 3', id: 3},
+  ])
+
   const handleClick = () => {
     setName('Hao')
     console.log(name)
@@ -12,6 +18,12 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
