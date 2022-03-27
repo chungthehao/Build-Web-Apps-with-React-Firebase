4e89b3157098000b22bfa32554eca0e1f7c7467f
Henry Chung <chungthehao@gmail.com>
2022-03-27 14:52:39 +0700

    Using the Previous State

--- first-review-react-app/src/App.js
+++ first-review-react-app/src/App.js
@@ -2,26 +2,31 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState('Henry')
   const [events, setEvents] = useState([
     {title: 'Event 1', id: 1},
     {title: 'Event 2', id: 2},
     {title: 'Event 3', id: 3},
   ])
 
-  const handleClick = () => {
-    setName('Hao')
-    console.log(name)
+  const handleClick = (id) => {
+    console.log(id)
+
+    // setEvents ko cập nhật liền, tới lúc nó cập nhật events có thể đã bị chỗ khác thay đổi giá trị rồi ==> bị cũ
+    // setEvents(events.filter((event) => {
+    //   return event.id !== id
+    // }))
+
+    setEvents((prevEvents) => prevEvents.filter((event) => {
+      return event.id !== id
+    }))
   }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
-
       {events.map((event, index) => (
         <div key={event.id}>
           <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>Delete event</button>
         </div>
       ))}
     </div>
