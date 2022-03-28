f2f1d105466677f9950b6b85abd584659e4f959d
Henry Chung <chungthehao@gmail.com>
2022-03-28 17:52:49 +0700

    controlled inputs

--- first-review-react-app/src/components/NewEventForm.js
+++ first-review-react-app/src/components/NewEventForm.js
@@ -6,26 +6,35 @@ export default function NewEventForm() {
   const [title, setTitle] = useState('')
   const [date, setDate] = useState('')
 
-  // const handleChange = e => {
-  //   setTitle(e.target.value)
-  // }
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
+  }
 
   return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={e => setTitle(e.target.value)} />
+        <input 
+          type="text" 
+          onChange={e => setTitle(e.target.value)} 
+          value={title} />
       </label>
 
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={e => setDate(e.target.value)} />
+        <input 
+          type="date" 
+          onChange={e => setDate(e.target.value)} 
+          value={date} />
       </label>
 
       <button>Submit</button>
 
       <p>Title - {title}</p>
       <p>Date - {date}</p>
+
+      <p onClick={resetForm}>Reset the form</p>
     </form>
   )
 }
