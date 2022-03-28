0630181f9a4df6aac0c61b5c902f06cc22803ba2
Henry Chung <chungthehao@gmail.com>
2022-03-28 21:37:28 +0700

    Submitting forms

--- first-review-react-app/src/components/NewEventForm.js
+++ first-review-react-app/src/components/NewEventForm.js
@@ -11,8 +11,22 @@ export default function NewEventForm() {
     setDate('')
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const event = {
+      title: title,
+      date: date,
+      id: Math.floor(Math.random() * 1000000)
+    }
+
+    console.log(event)
+
+    resetForm()
+  }
+
   return (
-    <form className="new-event-form">
+    <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
         <input 
@@ -30,11 +44,6 @@ export default function NewEventForm() {
       </label>
 
       <button>Submit</button>
-
-      <p>Title - {title}</p>
-      <p>Date - {date}</p>
-
-      <p onClick={resetForm}>Reset the form</p>
     </form>
   )
 }
