09c760704c1d06819084e311644eede9bc5e65c4
Henry Chung <chungthehao@gmail.com>
2022-03-30 15:52:26 +0700

    Fetching data with a useEffect hook

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import './TripList.css'
 
 export default function TripList() {
   const [trips, setTrips] = useState([])
 
-  fetch("http://localhost:3000/trips")
-  .then(res => res.json())
-  .then(json => setTrips(json))
+  useEffect(() => {
+    fetch("http://localhost:3000/trips")
+      .then(res => res.json())
+      .then(json => setTrips(json))
+  }, [])
 
   console.log(trips)
 
   return (
-    <div>
-      <h2>TripList</h2>
+    <div className='trip-list'>
+      <h2>Trip List</h2>
+      <ul>
+        {trips.map(trip => 
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>)}
+      </ul>
     </div>
   )
 }
