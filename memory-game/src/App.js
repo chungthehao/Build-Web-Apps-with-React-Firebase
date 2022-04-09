7990107d03a9f9c3357370a20c532127bbeedd79
Henry Chung <chungthehao@gmail.com>
2022-04-09 23:32:07 +0700

    Making card choices

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -24,7 +26,11 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns)
+  const handleSelect = selectedCard => {
+    choiceOne ? setChoiceTwo(selectedCard) : setChoiceOne(selectedCard)
+  }
+
+  console.log(choiceOne, choiceTwo)
 
   return (
     <div className="App">
@@ -33,7 +39,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card => (
-          <SingleCard card={card} key={card.id} />
+          <SingleCard card={card} key={card.id} handleClick={handleSelect} />
         ))}
       </div>
     </div>
--- memory-game/src/components/SingleCard.js
+++ memory-game/src/components/SingleCard.js
@@ -1,10 +1,15 @@
 import './SingleCard.css'
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleClick }) {
   return (
     <div className="card">
       <img src={card.src} alt="front card" className="front" />
-      <img src='/img/cover.png' alt="back card" className="back" />
+      <img 
+        src='/img/cover.png' 
+        alt="back card" 
+        className="back" 
+        onClick={() => handleClick(card)}
+      />
     </div>
   )
 }
