68a32681593e7aecfb29310155a9f170d2274fc4
Henry Chung <chungthehao@gmail.com>
2022-04-16 15:45:02 +0700

    Use useParams hook to get params from the url

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,7 +1,23 @@
+import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
 
 
 export default function Article() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/articles/' + id
+  const { data: article, error, isPending } = useFetch(url)
+
   return (
-    <h3>Article page</h3>
+    <>
+      {isPending && <div>LOADING...</div>}
+      {error && <div>{error}</div>}
+      {article && (
+        <div>
+          <h2>{article.title}</h2>
+          <p>By {article.author}</p>
+          <p>{article.body}</p>
+        </div>
+      )}
+    </>
   )
 }
