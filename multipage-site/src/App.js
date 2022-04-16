1d84b4d47d77bfca3a4b5bb718fc42efa5668ab3
Henry Chung <chungthehao@gmail.com>
2022-04-16 16:18:58 +0700

    Using Redirect component to redirect for all not-found pages

--- multipage-site/src/App.js
+++ multipage-site/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, NavLink, Redirect, Route, Switch } from 'react-router-dom'
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -32,6 +32,10 @@ function App() {
 
           <Route path='/articles/:id'>
             <Article />
+          </Route>
+
+          <Route path='*'>
+            <Redirect to='/' />
           </Route>
         </Switch>
       </BrowserRouter>
