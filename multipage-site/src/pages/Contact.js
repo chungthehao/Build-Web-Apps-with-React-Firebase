679501d6c389f91430caa7230cade43ebdab55ac
Henry Chung <chungthehao@gmail.com>
2022-04-16 16:30:25 +0700

    Query params

--- multipage-site/src/pages/Contact.js
+++ multipage-site/src/pages/Contact.js
@@ -1,9 +1,15 @@
-
+import { useLocation } from 'react-router-dom'
 
 export default function Contact() {
+  const queryString = useLocation().search
+  console.log(queryString) // ?name=henry
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get('name')
+  console.log(name) // henry
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, contact us here...</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, temporibus facere officiis sunt debitis illum natus vero magnam voluptatem, numquam ea deleniti doloribus possimus maxime minus, ad assumenda iure delectus?</p>
     </div>
   )
