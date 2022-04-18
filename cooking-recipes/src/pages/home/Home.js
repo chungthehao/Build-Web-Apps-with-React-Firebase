a33f7030d8e3a75c64fc77d0b144be2b73791920
Henry Chung <chungthehao@gmail.com>
2022-04-18 21:18:54 +0700

    Fetching recipes

--- cooking-recipes/src/pages/home/Home.js
+++ cooking-recipes/src/pages/home/Home.js
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
 import './Home.css'
 
 
 export default function Home() {
+  const { data: recipes, error, isPending } = useFetch('http://localhost:3000/recipes')
+  
   return (
-    <div>Home</div>
+    <div className='home'>
+      {isPending && <p className='loading'>LOADING...</p>}
+      {error && <p className='error'>{error}</p>}
+      {recipes && recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h2>{recipe.title}</h2>
+          <p>{recipe.cookingTime}</p>
+          <Link to={`/recipes/${recipe.id}`}>Read more...</Link>
+        </div>
+      ))}
+    </div>
   )
 }
