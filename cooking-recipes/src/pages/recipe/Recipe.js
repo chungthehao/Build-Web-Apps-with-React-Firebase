81e1dd6e3ce4eeaa9f18108e0af6735cdfb94289
Henry Chung <chungthehao@gmail.com>
2022-04-18 22:09:51 +0700

    Recipe component

--- cooking-recipes/src/pages/recipe/Recipe.js
+++ cooking-recipes/src/pages/recipe/Recipe.js
@@ -1,8 +1,18 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
 import './Recipe.css'
 
 
 export default function Recipe() {
+  const { id } = useParams()
+  const { data: recipe, error, isPending } = useFetch('http://localhost:3000/recipes/' + id)
+
   return (
-    <div>Recipe</div>
+    <div className="recipe">
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>LOADING ...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
   )
 }
