c2642e590e1fd827f3f58936c7ae116c2ea981d3
Henry Chung <chungthehao@gmail.com>
2022-04-26 15:04:02 +0700

    Search result page

--- cooking-recipes/src/pages/search/Search.js
+++ cooking-recipes/src/pages/search/Search.js
@@ -1,8 +1,24 @@
+import { useLocation } from 'react-router-dom'
+import RecipeList from '../../components/RecipeList'
+import { useFetch } from '../../hooks/useFetch'
+
 import './Search.css'
 
 
 export default function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+  const url = 'http://localhost:3000/recipes?q=' + query
+
+  const { data, error, isPending } = useFetch(url)
+
   return (
-    <div>Search</div>
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {isPending && <p className='loading'>Loading...</p>}
+      {error && <p className='error'>{error}</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
   )
 }
