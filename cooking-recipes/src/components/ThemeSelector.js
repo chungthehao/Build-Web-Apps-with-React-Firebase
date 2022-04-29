7cc9e6963265e6ab9f3262300f000f4a4a31f2e0
Henry Chung <chungthehao@gmail.com>
2022-04-29 19:26:34 +0700

    dark/light mode selector

--- cooking-recipes/src/components/ThemeSelector.js
+++ cooking-recipes/src/components/ThemeSelector.js
@@ -1,14 +1,25 @@
 import { useTheme } from '../hooks/useTheme'
 
+import modeIcon from '../assets/mode-icon.svg'
 import './ThemeSelector.css'
 
 const themeColors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme()
+  const { changeColor, changeMode, mode } = useTheme()
+
+  console.log(mode)
 
   return (
     <div className="theme-selector">
+      <div className="mode-toggler">
+        <img 
+          src={modeIcon} 
+          alt="dark/light mode toggler"  
+          onClick={() => changeMode(mode === 'dark' ? 'light' : 'dark')}  
+          style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(10%)' }}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map(color => 
           <div 
