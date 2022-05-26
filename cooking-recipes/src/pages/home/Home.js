a59211248729d38fcfdd7be33155aace5b36491d
Henry Chung <chungthehao@gmail.com>
2022-05-26 19:01:21 +0700

    Fetch a 'recipes' collection

--- cooking-recipes/src/pages/home/Home.js
+++ cooking-recipes/src/pages/home/Home.js
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react'
 import RecipeList from '../../components/RecipeList'
-import { useFetch } from '../../hooks/useFetch'
+import { projectFirestore } from '../../firebase/config'
 
 import './Home.css'
 
 
 export default function Home() {
-  const { data: recipes, error, isPending } = useFetch('http://localhost:3000/recipes')
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState('')
+  const [recipes, setRecipes] = useState(null)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').get().then(snapshot => {
+      if (snapshot.empty) {
+        setError('Recipes not found.')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        setRecipes(results)
+        setIsPending(false)
+      }
+    }).catch(err => {
+      setError(err.message)
+      setIsPending(false)
+    })
+  }, [])
   
   return (
     <div className='home'>
