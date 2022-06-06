189fd701cb9200b9d6089bd0ab84f0bc0094a84a
Henry Chung <chungthehao@gmail.com>
2022-06-06 21:50:30 +0700

    Home, Login, Signup pages & their routes

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Switch  } from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
