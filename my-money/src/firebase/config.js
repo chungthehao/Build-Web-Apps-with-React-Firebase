02337a2ff6e831590559186839d5610d5d1951fb
Henry Chung <chungthehao@gmail.com>
2022-06-07 18:19:45 +0700

    Setup firebase

---
+++ my-money/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABbhrjbn6nTa9Q8s8gPxG2l009GjoRxEk",
+  authDomain: "my-money-168.firebaseapp.com",
+  projectId: "my-money-168",
+  storageBucket: "my-money-168.appspot.com",
+  messagingSenderId: "138366710489",
+  appId: "1:138366710489:web:938b55d7bcbf3def9b04c8"
+}
+
+// Init firebase
+firebase.initializeApp(firebaseConfig)
+
+// Init firestore
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
