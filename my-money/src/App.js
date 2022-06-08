308b5f5aa316880eec4eb289ffe7a1edfa9ed896
Henry Chung <chungthehao@gmail.com>
2022-06-08 18:22:25 +0700

    Waiting until auth is ready before rendering App's content

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,27 +1,32 @@
 import { BrowserRouter, Route, Switch  } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
+import { useAuthContext } from './hooks/useAuthContext';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 
 
 function App() {
+  const { authIsReady } = useAuthContext()
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/signup'>
-            <Signup />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <div className="App">    
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
