5e1c749eecdf723e76f9fa7de2c02bc1be29ee8c
Henry Chung <chungthehao@gmail.com>
2022-06-08 17:15:12 +0700

    Check the Firebase authentication when first loaded

--- my-money/src/context/AuthContext.js
+++ my-money/src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react"
+import { createContext, useEffect, useReducer } from "react"
+import { projectAuth } from "../firebase/config"
 
 
 const AuthContext = createContext()
@@ -9,13 +10,31 @@ const authReducer = (state, action) => {
       return { ...state, user: action.payload }
     case 'LOGOUT':
       return { ...state, user: null }
+    case 'AUTH_IS_READY':
+      return { ...state, user: action.payload, authIsReady: true }
     default: 
       return state
   }
 }
 
 const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null })
+  const [state, dispatch] = useReducer(
+    authReducer, 
+    { user: null, authIsReady: false }
+  )
+
+  // Check the authentication with firebase when the web is first loaded.
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((firebaseUser) => {
+      dispatch({
+        type: 'AUTH_IS_READY',
+        payload: firebaseUser
+      })
+
+      // onAuthStateChanged run the callback for the 1st time when web first loaded, and when the user signs in/out, we only need to run the callback once
+      unsub() 
+    })
+  }, [])
 
   console.log('AuthContext - STATE', state)
 
