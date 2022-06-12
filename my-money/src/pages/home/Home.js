bed7abe5bce32885fd918dcaa3df74b02084ba47
Henry Chung <chungthehao@gmail.com>
2022-06-12 15:13:13 +0700

    TransactionList comp

--- my-money/src/pages/home/Home.js
+++ my-money/src/pages/home/Home.js
@@ -1,14 +1,22 @@
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 import styles from './Home.module.css'
 import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 
 function Home() {
     const { user } = useAuthContext()
+    // console.log(user.uid)
+    const { documents, error } = useCollection(
+        'transactions', 
+    )
+    // console.log(documents, error)
 
     return (  
         <div className={styles.container}>
             <div className={styles.content}>
-                A list of transactions
+                {error && <p>{error}</p>}
+                {documents && <TransactionList transactions={documents} />}
             </div>
             <div className={styles.sidebar}>
                 <TransactionForm uid={user.uid} />
