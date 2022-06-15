15bb2b3c6a4ed7dde31571ceef64632ad2a9fcd0
Henry Chung <chungthehao@gmail.com>
2022-06-15 14:07:32 +0700

    Firebase setup

---
+++ the-tuesday-app/src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkTyvNmV_hcybEyTMvFoWpe1bx5b9-yhI",
+  authDomain: "the-tuesday-app.firebaseapp.com",
+  projectId: "the-tuesday-app",
+  storageBucket: "the-tuesday-app.appspot.com",
+  messagingSenderId: "209819289051",
+  appId: "1:209819289051:web:d45b2c096e0ad1a26c93fe"
+}
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// Timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
