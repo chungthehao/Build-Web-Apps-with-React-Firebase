308c7683e9aaf6d8ba720f848983165c8efcb323
Henry Chung <chungthehao@gmail.com>
2022-06-17 21:01:55 +0700

    Firebase storage config setup

--- the-tuesday-app/src/firebase/config.js
+++ the-tuesday-app/src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBkTyvNmV_hcybEyTMvFoWpe1bx5b9-yhI",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig)
 // Init services
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 
 // Timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { projectFirestore, projectAuth, timestamp }
+export { projectFirestore, projectAuth, projectStorage, timestamp }
