c33e8656e31429b1484b695f3889a34d1090ec5f
Henry Chung <chungthehao@gmail.com>
2022-06-20 21:54:10 +0700

    Conditional navbar links

--- the-tuesday-app/src/components/Navbar.js
+++ the-tuesday-app/src/components/Navbar.js
@@ -2,9 +2,11 @@ import './Navbar.css'
 import Temple from '../assets/temple.svg'
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Navbar() {
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
 
   return ( <div className="navbar">
     <ul>
@@ -13,12 +15,18 @@ function Navbar() {
         <span>Tuesday App</span>
       </li>
 
-      <li><Link to='/login'>Login</Link></li>
-      <li><Link to='/signup'>Signup</Link></li>
-      <li>
-        {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-        {isPending && <button className="btn" disabled>Logging out...</button>}
-      </li>
+      {!user && (
+        <>
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/signup'>Signup</Link></li>
+        </>
+      )}
+      {user &&
+        <li>
+          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+          {isPending && <button className="btn" disabled>Logging out...</button>}
+        </li>
+      }
     </ul>
   </div> );
 }
