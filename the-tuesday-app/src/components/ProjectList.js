936453edc7f8fcc03092d8198535018eb4446b6c
Henry Chung <chungthehao@gmail.com>
2022-06-28 20:53:08 +0700

    Making the project list

--- the-tuesday-app/src/components/ProjectList.js
+++ the-tuesday-app/src/components/ProjectList.js
@@ -1,13 +1,25 @@
+import { Link } from 'react-router-dom';
+import Avatar from './Avatar';
 import './ProjectList.css'
 
 function ProjectList({ projects }) {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 && <p>No project yet!</p>}
       {projects.map(proj => (
-        <div key={proj.id}>
-          {proj.name}
-        </div>
+        <Link key={proj.id} to={`/projects/${proj.id}`}>
+          <h4>{proj.name}</h4>
+          <p>Due by {proj.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {proj.assignedUsersList.map(u => (
+                <li key={u.id}>
+                  <Avatar src={u.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
