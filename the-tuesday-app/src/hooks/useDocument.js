b3fcac9cbff6c1a2e5114a52425c1c8ccc5064d0
Henry Chung <chungthehao@gmail.com>
2022-06-28 20:53:26 +0700

    Making useDocument hook

---
+++ the-tuesday-app/src/hooks/useDocument.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+
+export const useDocument = (collectionName, documentId) => {
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setError(null) // Reset
+    setDocument(null) // Reset
+    const ref = projectFirestore.collection(collectionName).doc(documentId)
+
+    // Get realtime data
+    const unsubscribe = ref.onSnapshot(snapshot => {
+      if (snapshot.exists) {
+        setDocument({ ...snapshot.data(), id: snapshot.id })
+      }
+    }, err => {
+      console.log(err.message)
+      setError('Failed to get the document.')
+    })
+
+    // Cleanup function
+    return () => unsubscribe()
+  }, [collectionName, documentId])
+
+  return { document, error }
+}
