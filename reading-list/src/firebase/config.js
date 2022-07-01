d04b37892ef15fb51ea99b080e61cae87ae4723f
Henry Chung <chungthehao@gmail.com>
2022-07-01 16:48:22 +0700

    Firebase config file

---
+++ reading-list/src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwlZb4fVd9vzd3GD8iA6LM091pTp5g5yE",
+  authDomain: "reading-list-firebase9.firebaseapp.com",
+  projectId: "reading-list-firebase9",
+  storageBucket: "reading-list-firebase9.appspot.com",
+  messagingSenderId: "935941362871",
+  appId: "1:935941362871:web:32c592bf520e852d622932"
+}
+
+// Init Firebase
+initializeApp(firebaseConfig)
+
+// Init Firestore
+const db = getFirestore()
+
+export { db  }
